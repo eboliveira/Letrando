7c3aaaec282870a037dade268192ac998f01e51c
rmassarotto <r_massarotto@hotmail.com>
2018-09-25 08:25:46 -0300

    Requisicao de palavras

--- src/main/kotlin/letrando/Letrando.kt
+++ src/main/kotlin/letrando/Letrando.kt
@@ -1,17 +1,24 @@
 package letrando
 
+import com.google.gson.JsonArray
 import com.google.gson.JsonParser
+import javafx.beans.property.SimpleObjectProperty
 import javafx.beans.property.SimpleStringProperty
+import javafx.collections.FXCollections
 import javafx.scene.control.Alert
+import javafx.scene.control.TextField
 import javafx.scene.paint.Color
 import tornadofx.*
 import khttp.*;
+import java.util.*
 
 data class Player(var name :String?= null, var date : String?=null, var score : Int ?= null)
+data class Words(var numeroJogo :Int ?= null, var words :JsonArray ?= null)
 
 class Letrando:App(MyView::class)
 
 var allPlayersList = mutableListOf<Player>()  //lista contendo todos os players que estão no banco (definida globalmente para a tableview)
+var allGameWords = mutableListOf<Words>()
 
 class dbController(){
     init {}
@@ -28,6 +35,21 @@ class dbController(){
             playerAux.score = allPlayersJsonArray.get(i).asJsonObject.get("score").asInt
             playerAux.date = allPlayersJsonArray.get(i).asJsonObject.get("date").asString
             allPlayersList.add(playerAux)
+        }
+    }
+    fun getWords(){
+        val resp = get("http://localhost:8080/words")
+        val allWords = resp.text
+        val parser = JsonParser()
+        val allWordsJson = parser.parse(allWords)
+        var allWordsJsonArray = allWordsJson.asJsonArray
+        allGameWords.clear()
+        for (i in 0 until allWordsJsonArray.size()){
+            var wordAux = Words()
+            wordAux.numeroJogo = allWordsJsonArray.get(i).asJsonObject.get("numeroJogo").asInt
+            wordAux.words = allWordsJsonArray.get(i).asJsonObject.get("words").asJsonArray
+            allGameWords.add(wordAux)
+
         }
     }
 }
@@ -51,7 +73,7 @@ class Records:View(){       //view para mostrar as pontuações
             }
         }
     }
-    }
+}
 
 class MyView:View(){        //view inicial do jogo
     override val root = vbox()  //linha padrão do tornadofx, vbox(preenche com componentes verticalmente) pode ser substituido por outras views
@@ -92,7 +114,9 @@ class MyView:View(){        //view inicial do jogo
                             }
                             //verificação se o nome já existe aqui
                             else{
-                                //jogo aqui
+                                //var controller = dbController()
+                                //controller.getWords()
+                                replaceWith<Game>()
                             }
                         }
                     }
@@ -105,6 +129,116 @@ class MyView:View(){        //view inicial do jogo
                     }
                 }
             }
+        }
+    }
+}
+
+private fun removeUltimoChar(str: String): String {
+    return str.substring(0, str.length - 1)
+}
+
+object Stopwatch {
+    inline fun elapse(callback: () -> Unit): Long {
+        var start = System.currentTimeMillis()
+        callback()
+        return System.currentTimeMillis() - start
+    }
+
+    inline fun elapseNano(callback: () -> Unit): Long {
+        var start = System.nanoTime()
+        callback()
+        return System.nanoTime() - start
+    }
+}
+
+class Game:View(){       //view do jogo
+    override val root = vbox()
+
+    init {
+        with(root){
+
+            style{
+                backgroundColor += c("#88ff88")
+            }
+            var words: MutableList<String> = mutableListOf<String>("hospital",
+                    "ilhotas",
+                    "palitos",
+                    "sol",
+                    "tio",
+                    "sal")
+
+            //println(allGameWords)
+            var acertos = FXCollections.observableArrayList("Palavras:")
+            var palavraDoJogo = words.first().toCharArray().toMutableList().shuffled()
+            var tentativa = ""
+
+            label {
+                children.bind(acertos){
+                    label(it){
+                        style {
+                            fontSize = 25.px
+                        }
+                    }
+                }
+            }
+
+            val campoTexto = label(){
+                paddingLeft = 300
+                paddingTop = 200
+                style {
+                    fontSize = 50.px
+                }
+            }
+
+            hbox {
+                paddingLeft = 160
+                paddingTop = 30
+
+                for (letra in palavraDoJogo) {
+                    togglebutton(letra.toString().capitalize()) {
+                        paddingAll = 20
+                        action {
+                            if (!isSelected) {
+                                tentativa += letra.toString()
+                                campoTexto.setText(tentativa)
+                                println(tentativa)
+                            } else {
+                                tentativa = removeUltimoChar(tentativa)
+                                campoTexto.setText(tentativa)
+                                println(tentativa)
+                            }
+                        }
+                    }
+                }
+
+                button("Enviar") {
+                    paddingAll = 20
+                    action {
+                        campoTexto.getText()
+
+                        val iterator = words.iterator()
+
+                        while (iterator.hasNext()) {
+                            val item = iterator.next()
+                            if (item == campoTexto.getText()) {
+                                acertos.add(item)
+                                iterator.remove()
+                                println("Acertou")
+                                if(words.isEmpty()){
+                                    alert(Alert.AlertType.INFORMATION, "Fim de jogo!")
+                                    replaceWith<MyView>()
+                                }
+                                break
+                            } else {
+                                if (!iterator.hasNext())
+                                    println("Errou")
+                            }
+                        }
+
+                    }
+                }
+            }
+
         }
     }
 }
--- src/main/kotlin/letrando/Server.kt
+++ src/main/kotlin/letrando/Server.kt
@@ -7,6 +7,7 @@ import java.io.PrintWriter
 import java.net.InetSocketAddress
 import org.litote.kmongo.*;
 import letrando.Player
+import letrando.Words
 import java.time.LocalDate
 
 /**
@@ -16,6 +17,7 @@ fun main(args: Array<String>) {
     val client = KMongo.createClient() //get com.mongodb.MongoClient new instance
     val database = client.getDatabase("Letrando") //normal java driver usage
     val players = database.getCollection<Player>("player")
+    val words = database.getCollection<Words>("words")
 
     HttpServer.create(InetSocketAddress(8080), 0).apply {
         createContext("/records") { http -> //rota do get
@@ -31,6 +33,21 @@ fun main(args: Array<String>) {
                 out.println(playersList)
             }
         }
+
+        createContext("/words") { http -> //rota do get
+            http.responseHeaders.add("Content-type", "text/plain")
+            val parser = JsonParser()
+            http.sendResponseHeaders(200, 0)
+            val wordsIterable = words.find()
+            var wordsList = JsonArray()
+            for(item in wordsIterable){
+                wordsList.add(parser.parse(item.json))
+            }
+            PrintWriter(http.responseBody).use { out ->
+                out.println(wordsList)
+            }
+        }
+
         createContext("/verify") { http -> //rota do post
             http.responseHeaders.add("Content-type", "text/plain")
             val resp = http.requestBody.readAllBytes()
@@ -39,7 +56,7 @@ fun main(args: Array<String>) {
             var respJson =  parser.parse(respString).asJsonObject
             var exists = players.findOne(Player::name eq respJson.get("name").asString)
             if (exists == null){
-//                players.insertOne(Player(respJson.get("name").asString, LocalDate.now().toString(),0))
+                //players.insertOne(Player(respJson.get("name").asString, LocalDate.now().toString(),0))
                 http.sendResponseHeaders(200, 0)
                 PrintWriter(http.responseBody).use { out ->
                     out.println("Avaible")
